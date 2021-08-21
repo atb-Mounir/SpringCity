1e73275de5734d8b934a7cd4beaef72ca6c7d62a
atb Mounir <aitbmohakim@gmail.com>
2021-08-21 11:36:44 +0200

    import du projet généré avec Spring initializr

---
+++ src/main/java/com/eni/SpringCity/SpringCityApplication.java
@@ -0,0 +1,13 @@
+package com.eni.SpringCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eni/SpringCity/SpringCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.SpringCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
